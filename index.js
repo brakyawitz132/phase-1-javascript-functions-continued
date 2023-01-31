f83a80d9ed21cee01de09d5d91ab6e47a7797376
Jacob Park <jacobypark122@gmail.com>
2023-01-31 13:45:22 -0500

    created functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity) {
+    // return `This Saturday, I want to ${activity}!`
+
+    switch (arguments.length) {
+        case 1:
+            return `This Saturday, I want to ${activity}!`
+        default:
+            return "This Saturday, I want to roller-skate!"
+    }
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function (adj="special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
